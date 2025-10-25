51e5e7da2be928e9cbfa54f793b87024f76b710c
Mazzberry <h.mohammadreza736@gmail.com>
2025-10-25 19:34:30 +0330

    fC

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+
+	// zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Print("test")
+	log.Info().Str("category", "search").Int("Duration time", 80).Msg("searching for a thing")
+
+	err := errors.New("this is an err")
+
+	log.Error().Err(err).Msg("error occurred")
+
+	err = func3()
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("Error occurred (func3 error)")
+	}
+
+}
+
+func func1() error{
+	return errors.New("this is and error (func1)")
+}
+
+func func2() error{
+	err := func1()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func func3() error{
+	err := func2()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// trace
+// debug
+// info
+// warn
+// error
+// fatal(critical)
+// panic
